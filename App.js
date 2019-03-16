24e0ec399c110321d24b5b5e51272833ba5b7a92
AhmedShab <a.shaban@qetaa.com>
2019-03-16 17:35:45 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
+
+import Input from './src/components/Input/Input';
+import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions';
+
+class App extends Component {
+
+  placeAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
+  };
+
+  placeDeletedHandler = () => {
+    this.props.onDeletePlace();
+  };
+
+  modalClosedHandler = () => {
+    this.props.onDeselectPlace();
+  }
+
+  placeSelectedHandler = key => {
+    this.props.onSelectPlace(key);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace={this.props.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <Input onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.props.places}
+          onItemDeleted={this.placeSelectedHandler} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    justifyContent: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
