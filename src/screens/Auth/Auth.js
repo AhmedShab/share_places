08dc94fd764641bea85a2d35112802366eed593f
AhmedShab <a.shaban@qetaa.com>
2019-05-19 08:44:42 +0300

    start styling the auth file

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
@@ -8,12 +8,32 @@ class AuthScreen extends Component {
 	}
 	render() {
 		return (
-			<View>
-				<Text>Auth Screen</Text>
-				<Button title="Login" onPress={this.loginHandler} />
+			<View style={styles.conatiner}>
+				<Text>Please log in</Text>
+				<Button title="Switch to Login" />
+				<View style={styles.inputContainer}>
+					<TextInput style={styles.input} placeholder="Your e-Mail Address" />
+					<TextInput style={styles.input} placeholder="Password" />
+					<TextInput style={styles.input} placeholder="confirm Password" />
+				</View>
+				<Button title="Submit" onPress={this.loginHandler} />
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	conatiner: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	inputContainer: {
+		width: "80%"
+	},
+	input: {
+		width: "100%"
+	}
+})
 
 export default AuthScreen;
