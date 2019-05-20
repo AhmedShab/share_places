1ec1dd6c16e4976c41a3bf32fd2664e54a880459
AhmedShab <a.shaban@qetaa.com>
2019-05-20 08:20:04 +0300

    add background image

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, ImageBackground } from 'react-native';
 import startTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
 	loginHandler = () => {
@@ -11,18 +12,20 @@ class AuthScreen extends Component {
 	}
 	render() {
 		return (
-			<View style={styles.conatiner}>
-				<MainText>
-					<HeadingText>Please log in</HeadingText>
-				</MainText>
-				<Button title="Switch to Login" />
-				<View style={styles.inputContainer}>
-					<DefaultInput style={styles.input} placeholder="Your e-Mail Address" />
-					<DefaultInput style={styles.input} placeholder="Password" />
-					<DefaultInput style={styles.input} placeholder="confirm Password" />
+			<ImageBackground style={styles.backgroundImage} source={backgroundImage}>
+				<View style={styles.conatiner}>
+					<MainText>
+						<HeadingText>Please log in</HeadingText>
+					</MainText>
+					<Button title="Switch to Login" />
+					<View style={styles.inputContainer}>
+						<DefaultInput style={styles.input} placeholder="Your e-Mail Address" />
+						<DefaultInput style={styles.input} placeholder="Password" />
+						<DefaultInput style={styles.input} placeholder="confirm Password" />
+					</View>
+					<Button title="Submit" onPress={this.loginHandler} />
 				</View>
-				<Button title="Submit" onPress={this.loginHandler} />
-			</View>
+			</ImageBackground>
 		);
 	}
 }
@@ -32,6 +35,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center"
+	},
+	backgroundImage: {
+		flex: 1,
+		width: "100%"
 	},
 	inputContainer: {
 		width: "80%",
