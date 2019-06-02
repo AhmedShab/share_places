af6c10f0b4b086f4d8832aeb3f30b81d1d83f255
AhmedShab <a.shaban@qetaa.com>
2019-06-02 13:17:34 +0300

    show google maps from the find place image

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
-import { View, Image, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import {
+	View,
+	Image,
+	Text,
+	Dimensions,
+	StyleSheet,
+	TouchableOpacity
+} from 'react-native';
 import { connect } from 'react-redux';
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { deletePlace } from '../../store/actions';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 class placeDetail extends Component {
 	constructor(props) {
@@ -11,59 +19,82 @@ class placeDetail extends Component {
 		Dimensions.addEventListener('change', this.updateStyles);
 
 		this.state = {
-			viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
-		}
+			viewMode:
+				Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
+		};
 	}
 
 	updateStyles = dims => {
 		this.setState({
 			viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
-		})
-	}
-
+		});
+	};
 
 	placeDeletedHandler = () => {
 		this.props.onDeletePlace(this.props.selectedPlace.key);
 		this.props.navigator.pop();
-	}
+	};
 
 	render() {
 		return (
-			<View style={[
-				styles.container,
-				this.state.viewMode === 'portrait' ?
-					styles.portraitContainer :
-					styles.landscapeContainer
-			]} >
-				<View style={styles.subContainer}>
-					<Image style={styles.placeImage} source={this.props.selectedPlace.image} />
+			<View
+				style={[
+					styles.container,
+					this.state.viewMode === 'portrait'
+						? styles.portraitContainer
+						: styles.landscapeContainer
+				]}
+			>
+				<View style={styles.placeDetailContainer}>
+					<View style={styles.subContainer}>
+						<Image
+							style={styles.placeImage}
+							source={this.props.selectedPlace.image}
+						/>
+					</View>
+					<View style={styles.subContainer}>
+						<MapView
+							provider={PROVIDER_GOOGLE}
+							style={styles.map}
+							initialRegion={{
+								...this.props.selectedPlace.location,
+								latitudeDelta: 0.0122,
+								longitudeDelta:
+									(Dimensions.get('window').width /
+										Dimensions.get('window').height) *
+									0.0122
+							}}
+						>
+							<Marker
+								coordinate={this.props.selectedPlace.location}
+							/>
+						</MapView>
+					</View>
 				</View>
 				<View style={styles.subContainer}>
 					<View>
-						<Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+						<Text style={styles.placeName}>
+							{this.props.selectedPlace.name}
+						</Text>
 					</View>
 					<View>
 						<TouchableOpacity onPress={this.placeDeletedHandler}>
 							<View style={styles.deleteButton}>
-								<Icon
-									size={30}
-									name="ios-trash"
-									color="red"
-								/>
+								<Icon size={30} name='ios-trash' color='red' />
 							</View>
 						</TouchableOpacity>
 					</View>
 				</View>
 			</View>
 		);
-	};
-};
+	}
+}
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onDeletePlace: (key) => dispatch(deletePlace(key))
-	}
-}
+		onDeletePlace: key => dispatch(deletePlace(key))
+	};
+};
 
 const styles = StyleSheet.create({
 	container: {
@@ -71,26 +102,35 @@ const styles = StyleSheet.create({
 		margin: 22
 	},
 	placeImage: {
-		width: "100%",
-		height: 200
+		width: '100%',
+		height: '100%'
 	},
 	placeName: {
-		fontWeight: "bold",
-		textAlign: "center",
+		fontWeight: 'bold',
+		textAlign: 'center',
 		fontSize: 28
 	},
 	portraitContainer: {
-		flexDirection: "column"
+		flexDirection: 'column'
 	},
 	landscapeContainer: {
-		flexDirection: "row"
+		flexDirection: 'row'
 	},
 	deleteButton: {
-		alignItems: "center"
+		alignItems: 'center'
 	},
 	subContainer: {
 		flex: 1
+	},
+	placeDetailContainer: {
+		flex: 1
+	},
+	map: {
+		...StyleSheet.absoluteFillObject
 	}
 });
 
-export default connect(null, mapDispatchToProps)(placeDetail)
+export default connect(
+	null,
+	mapDispatchToProps
+)(placeDetail);
