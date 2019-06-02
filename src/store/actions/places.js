fdd638c51f4b4b294fc65e324087746dde372c23
AhmedShab <a.shaban@qetaa.com>
2019-06-02 15:00:29 +0300

    save image and location to firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,21 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+const baseUrl = 'https://share-places-bc171.firebaseio.com/';
 
 export const addPlace = (placeName, location, image) => {
-	return {
-		type: ADD_PLACE,
-		placeName,
-		location,
-		image
+	const placeData = {
+		name: placeName,
+		location
+	};
+	return dispatch => {
+		fetch(`${baseUrl}/places.json`, {
+			method: 'post',
+			body: JSON.stringify(placeData)
+		})
+			.catch(err => console.log(err))
+			.then(res => res.json())
+			.then(parsedRes => {
+				console.log(parsedRes);
+			});
 	};
 };
 
